43382450b5131ffe04351a8757943bfe442f0e79
Abed Mohese <edaod887@gmail.com>
2026-01-12 17:05:19 +0200

    update project files

--- js/main.js
+++ js/main.js
@@ -5,23 +5,27 @@ const reset = document.getElementById("resetBtn");
 const output = document.getElementById("output");
 
 function caesarCipher(text, shift) {
+  if (shift < 0) {
+    shift = 26 + (shift % 26);
+  }
+  
   shift = shift % 26;
+  
   return text
-    .split("") 
+    .split("")
     .map(char => {
       const code = char.charCodeAt(0);
       if (code >= 65 && code <= 90) {
         return String.fromCharCode(((code - 65 + shift) % 26) + 65);
-      } 
-      else if (code >= 97 && code <= 122) {
+      } else if (code >= 97 && code <= 122) {
         return String.fromCharCode(((code - 97 + shift) % 26) + 97);
-      } 
-      else {
+      } else {
         return char;
       }
     })
-    .join(""); 
+    .join("");
 }
+
 
 button.onclick = () =>{
     const text = input.value;
